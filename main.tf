be01b180a27a1b594b4acba8b27fa50326cd5a8a
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-20 17:22:37 +0200

    Init commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+locals {
+  main_image    = regex("^(?:(?P<url>[^/]+))?(?:(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", images.main)
+  webhook_image = regex("^(?:(?P<url>[^/]+))?(?:(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", images.webhook)
+}
+
+resource "helm_release" "this" {
+  name             = var.name
+  repository       = var.repository
+  chart            = var.chart
+  namespace        = var.namespace
+  create_namespace = var.create_namespace
+
+  values = var.values
+
+  set {
+    name  = "controller.image.registry"
+    value = local.main_image.url
+  }
+
+  set {
+    name  = "controller.image.image"
+    value = local.main_image.image
+  }
+
+  set {
+    name  = "controller.image.tag"
+    value = local.main_image.tag
+  }
+
+  set {
+    name  = "controller.image.digest"
+    value = "false"
+  }
+
+  set {
+    name  = "controller.admissionWebhooks.patch.image.registry"
+    value = local.webhook_image.url
+  }
+
+  set {
+    name  = "controller.admissionWebhooks.patch.image.image"
+    value = local.webhook_image.image
+  }
+
+  set {
+    name  = "controller.admissionWebhooks.patch.image.tag"
+    value = local.webhook_image.tag
+  }
+  set {
+    name  = "controller.admissionWebhooks.patch.image.digest"
+    value = "false"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  description = "Name of helm release"
+  type        = string
+  default     = "ingress-nginx"
+}
+
+variable "repository" {
+  description = "Url of repository"
+  type        = string
+  default     = "https://kubernetes.github.io/ingress-nginx"
+}
+
+variable "chart" {
+  description = "Chart of helm release"
+  type        = string
+  default     = "ingress-nginx"
+}
+
+variable "namespace" {
+  description = "namespace of helm release"
+  type        = string
+  default     = "ingress-nginx"
+}
+
+variable "create_namespace" {
+  description = "Create namespace ?"
+  type        = bool
+  default     = true
+}
+
+variable "images" {
+  description = "Map of images"
+  type        = map(string)
+  default     = {}
+}
+
+variable "values" {
+  description = "Values"
+  type        = list(any)
+  default     = []
+}
